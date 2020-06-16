aff1e1770ece005749d89c2108b10faa2acd6f37
maronin <final3n@hanmail.net>
2020-06-16 17:04:16 +0900

    types

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type User {\n  id: Int!\n  email: String!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String!\n  phoneNumber: String!\n  verifiedEmail: Boolean!\n  profilePhto: String!\n  fullName: String!\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastlat: Float\n  lastOrientation: Float\n  createdAt: String!\n  updatedAt: String!\n}\n\ntype Query {\n  user: User\n}\n"];
+export const typeDefs = ["type User {\n  id: Int!\n  email: String!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String!\n  phoneNumber: String!\n  verifiedEmail: Boolean!\n  profilePhto: String!\n  fullName: String!\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastlat: Float\n  lastOrientation: Float\n  createdAt: String!\n  updatedAt: String!\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  playload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -22,6 +22,16 @@ export interface User {
   lastLng: number | null;
   lastlat: number | null;
   lastOrientation: number | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Verification {
+  id: number;
+  target: string;
+  playload: string;
+  key: string;
+  used: boolean;
   createdAt: string;
   updatedAt: string;
 }
